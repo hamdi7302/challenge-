7f2b00bd5558806ef4238177caf45a9794ee5888
hamdi7302 <hamdi.bntiba@gmail.com>
2017-02-26 23:18:30 +0100

    clean

--- app/controllers/page_controller.rb
+++
@@ -1,7 +0,0 @@
-class PageController < ApplicationController
-  def home
-  @variable=4
-  end
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'menu/routes'
+  get 'menu' =>  'menu#routes'
 
-  get 'page/home'
-  get 'page/index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
